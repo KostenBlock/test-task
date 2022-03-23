ff64fbb41bb0ae8c232411f4f453975569b8888e
KostenBlock <kostenkosta@gmail.com>
2022-03-24 00:00:20 +0300

    started

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,11 @@ import '../styles/globals.css'
 import "~/components/ui-components/selectors/drop-menu/transition.css"
 import Default from "~/layouts/default";
 
-import {store} from "next/dist/build/output/store";
-import {Provider} from "react-redux";
-
 function MyApp({Component, pageProps}) {
     return (
-        <Provider store={store}>
-            <Default>
-                <Component {...pageProps} />
-            </Default>
-        </Provider>
+        <Default>
+            <Component {...pageProps} />
+        </Default>
     )
 }
 
--- store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import elementsSlice from "~/store/reducers/user.slice";
-
-export const store = configureStore({
-	reducer: {
-		user: elementsSlice
-	},
-});
